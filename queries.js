a6e09f76c0deb257bacc8b04e26d55693ea2146f
Yiqi Zhang <>
2020-04-29 23:19:58 -0400

    first commit

---
+++ queries.js
@@ -0,0 +1,78 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "yiqi",
+  host: "localhost",
+  database: "sentiment",
+  password: "password",
+  port: 5432,
+});
+
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createUser = (request, response) => {
+  const { name, sentiment_score } = request.body;
+
+  pool.query(
+    "INSERT INTO users (name, sentiment_score) VALUES ($1, $2)",
+    [name, sentiment_score],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${result.insertId}`);
+    }
+  );
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, sentiment_score } = request.body;
+
+  pool.query(
+    "UPDATE users SET name = $1, sentiment_score = $2 WHERE id = $3",
+    [name, sentiment_score, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
